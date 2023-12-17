49f9f5f96bdf326c7d0a4931e84d000133f80632
0xvikasrushi <0xvikas@gmail.com>
2023-12-17 15:33:52 +0530

    upto dynamic link generation

--- App.js
+++ App.js
@@ -1,30 +1,71 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable prettier/prettier */
 import React, {useState} from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 import dynamicLinks from '@react-native-firebase/dynamic-links';
+import Clipboard from '@react-native-clipboard/clipboard';
 
 const App = () => {
   const [generatelink, setGenerateLink] = useState('');
-  async function buildLink() {
-    const buildLink = await dynamicLinks().buildLink({
-      link: 'https://invertase.io',
-      domainUriPrefix: 'https://xyz.page.link',
-      analytics: {
-        campaign: 'banner',
-      },
-    });
 
-    // eslint-disable-next-line no-undef
-    return link;
-  }
+  const buildLink = async () => {
+    try {
+      const link = await dynamicLinks().buildLink({
+        link: 'https://play.google.com/store/apps/details?id=com.hicircle.circle&hl=en_IN&gl=US',
+        domainUriPrefix: 'https://hicircle.page.link',
+        analytics: {
+          campaign: 'banner',
+        },
+      });
+      console.log('Deep link', link);
+      // eslint-disable-next-line no-undef
+      setGenerateLink(link);
+    } catch (error) {
+      console.error('Error generateing link ', error);
+    }
+  };
 
   return (
-    // eslint-disable-next-line react-native/no-inline-styles
-    <View style={{flex: 1, justifyContent: 'center', alignContent: 'center'}}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
       <Text>{generatelink}</Text>
       <TouchableOpacity
-        // eslint-disable-next-line react-native/no-inline-styles
-        style={{width: 200, height: 50, borderRadius: 10, borderWidth: 1}}>
+        style={{
+          width: 200,
+          height: 50,
+          borderRadius: 10,
+          borderColor: 'white',
+          borderWidth: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginTop: 50,
+        }}
+        onPress={() => {
+          buildLink();
+        }}>
         <Text>Generate Deep Link</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{
+          width: 200,
+          height: 50,
+          borderRadius: 10,
+          borderColor: 'white',
+          borderWidth: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginTop: 50,
+        }}
+        onPress={() => {
+          Clipboard.setString(generatelink);
+        }}>
+        <Text>Copy Deep Link</Text>
       </TouchableOpacity>
     </View>
   );
